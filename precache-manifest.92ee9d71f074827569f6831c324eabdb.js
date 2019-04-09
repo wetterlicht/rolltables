699d3f3358c0af0927512014ad9af6471f60ef79
Matthias Nötzli <matthias.noetzli@gmail.com>
2019-04-09 13:49:49 +0200

    Updates

---
+++ precache-manifest.92ee9d71f074827569f6831c324eabdb.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = [
+  {
+    "revision": "df660ae3a887d702e14d",
+    "url": "/wetterlicht/rolltables/static/css/main.6e934d3f.chunk.css"
+  },
+  {
+    "revision": "df660ae3a887d702e14d",
+    "url": "/wetterlicht/rolltables/static/js/main.a9685b3d.chunk.js"
+  },
+  {
+    "revision": "8efed21ea414a9d6b5b3",
+    "url": "/wetterlicht/rolltables/static/js/runtime~main.cc2dbf63.js"
+  },
+  {
+    "revision": "9dac378ea20988f84df5",
+    "url": "/wetterlicht/rolltables/static/css/2.93a2a13e.chunk.css"
+  },
+  {
+    "revision": "9dac378ea20988f84df5",
+    "url": "/wetterlicht/rolltables/static/js/2.67264402.chunk.js"
+  },
+  {
+    "revision": "e6257a726a0cf6ec8c6fec22821c055f",
+    "url": "/wetterlicht/rolltables/static/media/fa-regular-400.e6257a72.woff2"
+  },
+  {
+    "revision": "427d721b86fc9c68b2e85ad42b69238c",
+    "url": "/wetterlicht/rolltables/static/media/fa-regular-400.427d721b.woff"
+  },
+  {
+    "revision": "dcce4b7fbd5e895561e18af4668265af",
+    "url": "/wetterlicht/rolltables/static/media/fa-regular-400.dcce4b7f.eot"
+  },
+  {
+    "revision": "65b9977aa23185e8964b36eddbce7a20",
+    "url": "/wetterlicht/rolltables/static/media/fa-regular-400.65b9977a.ttf"
+  },
+  {
+    "revision": "5e2f92123d241cabecf0b289b9b08d4a",
+    "url": "/wetterlicht/rolltables/static/media/fa-brands-400.5e2f9212.woff2"
+  },
+  {
+    "revision": "418dad87601f9c8abd0e5798c0dc1feb",
+    "url": "/wetterlicht/rolltables/static/media/fa-solid-900.418dad87.woff2"
+  },
+  {
+    "revision": "2ef8ba3410dcc71578a880e7064acd7a",
+    "url": "/wetterlicht/rolltables/static/media/fa-brands-400.2ef8ba34.woff"
+  },
+  {
+    "revision": "a7140145ebaaf5fb14e40430af5d25c4",
+    "url": "/wetterlicht/rolltables/static/media/fa-solid-900.a7140145.woff"
+  },
+  {
+    "revision": "a7b95dbdd87e0c809570affaf366a434",
+    "url": "/wetterlicht/rolltables/static/media/fa-brands-400.a7b95dbd.eot"
+  },
+  {
+    "revision": "98b6db59be947f563350d2284fc9ea36",
+    "url": "/wetterlicht/rolltables/static/media/fa-brands-400.98b6db59.ttf"
+  },
+  {
+    "revision": "5eb754ab7dbd2fee562360528db4c3c0",
+    "url": "/wetterlicht/rolltables/static/media/fa-regular-400.5eb754ab.svg"
+  },
+  {
+    "revision": "ff8d9f8adb0d09f11d4816a152672f53",
+    "url": "/wetterlicht/rolltables/static/media/fa-solid-900.ff8d9f8a.ttf"
+  },
+  {
+    "revision": "46e7cec623d8bd790d9fdbc8de2d3ee7",
+    "url": "/wetterlicht/rolltables/static/media/fa-solid-900.46e7cec6.eot"
+  },
+  {
+    "revision": "5bf145531213545e03ff41cd27df7d2b",
+    "url": "/wetterlicht/rolltables/static/media/fa-brands-400.5bf14553.svg"
+  },
+  {
+    "revision": "49279363201ed19a840796e05a74a91b",
+    "url": "/wetterlicht/rolltables/static/media/fa-solid-900.49279363.svg"
+  },
+  {
+    "revision": "ad45b3becad0c995a0df314baab21d3a",
+    "url": "/wetterlicht/rolltables/index.html"
+  }
+];
